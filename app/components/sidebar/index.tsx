6ec493c4a84e26b5e8ae903bc766ad74d79a57e7
Yasiru-Deshan <yasirudeshan3@gmail.com>
2024-08-12 01:50:37 +0530

    sidebar added

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,21 +1,48 @@
 import styles from './sidebar.module.css';
+import logo from './../../../public/images/aa624316428cd4498ccd9769187ec302.png';
+import Image from 'next/image';
+import HomeIcon from './../../../public/icons/Home2-Linear-32px.svg';
+import TasksIcon from '../../../public/icons/TaskSquare-Linear-32px.svg';
+import ReportIcon from '../../../public/icons/Diagram-Linear-32px.svg';
+import InsightIcon from '../../../public/icons/LampCharge-Linear-32px.svg';
+import InboxIcon from '../../../public/icons/NotificationBing-Linear-32px.svg';
+import SettingsIcon from '../../../public/icons/Setting2-Linear-32px.svg';
+
+const sidebar_items = [
+    {id: 1, title: 'Home', icon: HomeIcon},
+    {id: 2, title: 'Tasks', icon: TasksIcon},
+    {id: 3, title: 'Report', icon: ReportIcon},
+    {id: 4, title: 'Insights', icon: InsightIcon},
+    {id: 5, title: 'Inbox', icon: InboxIcon},
+    {id: 6, title: 'Settings', icon: SettingsIcon}
+ 
+]
 
 export default function SideBar() {
     return(
 
         <aside className={styles.container}>
-            <div className={styles.logo}>
+            <div className={styles.sidebarHeader}>
+                <Image 
+                    src={logo} 
+                    alt="Logo" 
+                    className={styles.logo} 
+                    />
+                Code 94 Labs
 
-            </div>
+                </div>
             <ul className={styles.sidebar}>
-            <li>Home</li>
-            <li>Tasks</li>
-            <li>Report</li>
-            <li>Insights</li>
-            <li>Inbox</li>
-            <li>Settings</li>
+                {sidebar_items.map((item)=>(
+                
+                    <li key={item.id}><Image
+                        src={item.icon}
+                        alt="Home"
+                        width={24}
+                        height={24}
+                    />{item.title}</li>     
+                ))}    
             </ul>
         </aside>
-  
+
     )
 }
